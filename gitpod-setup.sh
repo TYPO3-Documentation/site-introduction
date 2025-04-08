c77b8a140110443ef3d41348843b944644dc99b1
lina.wolf <lwolf@w-commerce.de>
2025-04-08 14:42:11 +0200

    [FEATURE] GitPod introduction

--- gitpod-setup.sh
+++ gitpod-setup.sh
@@ -21,7 +21,6 @@ vendor/bin/typo3 extension:setup
 
 # 5. Create demo users
 vendor/bin/typo3 setup:begroups:default -g Both
-vendor/bin/typo3 backend:user:create --username=j.doe --password=UnsaFe123! --groups=1 --admin --maintainer --email=j.doe@example.org --no-interaction
 vendor/bin/typo3 backend:user:create --username=e.doe --password=UnsaFe123! --groups=1 --email=e.doe@example.org --no-interaction
 vendor/bin/typo3 backend:user:create --username=a.doe --password=UnsaFe123! --groups=1,2 --email=a.doe@example.org --no-interaction
 echo "User e.doe demonstrates standard editor permissions, a.doe advanced editor permissions"
